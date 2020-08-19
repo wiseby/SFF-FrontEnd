cbe69f0b017197409827507265a4e50c1da93592
Lucas WIseby <l.wiseby@gmail.com>
2020-08-19 16:21:36 +0200

    clean swipe

--- js/models/studio.js
+++
@@ -1,9 +0,0 @@
-export default class Studio {
-
-
-  constructor(studio) {
-    this.name = studio.name;
-    this.password = studio.password;
-    this.verified = studio.verified;
-  }
-}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-import { Movie } from './models/movie.js'
-
 var movieUrl = "http://localhost:5000/api/film";
 var studioUrl = "http://localhost:5000/api/filmstudio";
 var triviaUrl = "http://localhost:5000/api/filmTrivia";
@@ -12,9 +10,9 @@ let homeButton = document.getElementById("home-button");
 let moviesButton = document.getElementById("movies-button");
 let studiosButton = document.getElementById("studios-button");
 let logButton = document.getElementById("studios-button");
-loginButton.addEventListener("click", function() { console.log("login button pressed!") });
-registerButton.addEventListener("click", function() { console.log("register button pressed!") });
-homeButton.addEventListener("click", function() { console.log("home button pressed!") });
+loginButton.addEventListener("click", login());
+registerButton.addEventListener("click", register());
+homeButton.addEventListener("click", home());
 moviesButton.addEventListener("click", function() { console.log("movies button pressed!") });
 studiosButton.addEventListener("click", function() { console.log("studios button pressed!") });
 logButton.addEventListener("click", function() { console.log(movies) });
@@ -33,30 +31,20 @@ function renderMovieContent() {
   mainContent.insertAdjacentElement("beforeend", movieContent);
 }
 
-
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-
 function clearContent(element) {
   element.innerHTML = "";
 }
 
 function login() {
-
+  console.log("login feature");
 }
 
 function register() {
-
+  console.log("register feature");
 }
 
 function home() {
-
+  console.log("home feature");
 }
 
 
@@ -66,27 +54,13 @@ async function getMovies() {
   
   const triviaResponse = await fetch(triviaUrl);
   let fetchedTrivias = await triviaResponse.json();
-  
-  await fetchedMovies.forEach(movie => {
-    let trivias = [];
-    fetchedTrivias.forEach(item => {
-      if (item.filmId == movie.id) {
-        trivias.push(item);
-      }
-    });
-    let newMovie = new Movie(movie, trivias);
-    movies.push(newMovie);
-  });
-  movieContent = document.createElement("section")
-  movieContent.classList.add("movies");
-  movies.forEach(movie => {
-    movie.render(movieContent)
-    movie.detailButton.addEventListener("click", () => getMovie(movie.id), false);
-  });
-  renderMovieContent();
 };
 
 function getStudios() {
+
+}
+
+function getTrivias(movieId) {
 
 }
 
